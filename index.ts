3bed23fdf432285987f4f2f2164cdcc298900257
pathakravi <ravipathak0804@gmail.com>
2019-11-25 00:18:46 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -4,3 +4,19 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+//Duck Typing
+interface Duck {
+  walk : () => void;
+  swim : () => void;
+  quack : () => void;
+}
+
+let probablyADuck = {
+  walk : () => console.log('walk like a duck'),
+  swim : () => console.log('swim like a duck'),
+  quack : () => console.log('quack like a duck'),
+}
+
+function FlyOverWater(bird: Duck) { }
+FlyOverWater(probablyADuck);
