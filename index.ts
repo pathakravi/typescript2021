c8d180ec963928c31a5c485bc65ab4694f4589a3
pathakravi <ravipathak0804@gmail.com>
2019-11-25 01:00:24 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -7,19 +7,26 @@ appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 //Duck Typing
 interface Duck {
+  alpha: string;
+  beta?: string;
   walk : () => void;
   swim : () => void;
   quack : () => void;
 }
 
 let probablyADuck = {
+  alpha: 'Duck says alpha',
+  beta: 'Duck says beta',
+  gamma: 'Duck says gamma',
   walk : () => console.log('walk like a duck'),
   swim : () => console.log('swim like a duck'),
   quack : () => console.log('quack like a duck'),
 }
 
-function FlyOverWater(bird: Duck) { }
+function FlyOverWater(bird: Duck) {
+  console.log('In Duck typing, '+ bird.beta);
+ }
 FlyOverWater(probablyADuck);
+probablyADuck.walk();
 
-import { Category } from './enum';
 import { Book } from './interfaces';
