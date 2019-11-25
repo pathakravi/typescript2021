a25f33a8a16bcff807722a98b2d3dd238bed0a49
pathakravi <ravipathak0804@gmail.com>
2019-11-25 09:42:36 +0530

    1 file modified

--- utilityFunction.ts
+++ utilityFunction.ts
@@ -1,19 +1,17 @@
-namespace Utility {
-  export namespace Fees {
-    export function CalculateLateFee(daysLate: number): number {
-      return daysLate *25;
-    }
+export namespace Fees {
+  export function CalculateLateFee(daysLate: number): number {
+    return daysLate * 25;
   }
+}
 
-  export function MaxBooksAllowed(age: number): number {
-    if(age < 12) {
-      return 3;
-    } else {
-      return 10;
-    }
+export function MaxBooksAllowed(age: number): number {
+  if (age < 12) {
+    return 3;
+  } else {
+    return 10;
   }
+}
 
-  function privateFunc(): void {
-    console.log('This is private...');
-  }
+function privateFunc(): void {
+  console.log("This is private...");
 }
